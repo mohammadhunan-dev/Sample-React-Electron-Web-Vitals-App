85adf36e15bae788cda618ce8602510e16113858
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-05-05 20:35:28 -0400

    fix craco + add add readme

---
+++ public/electron.js
@@ -0,0 +1,17 @@
+const electron = require(electron);
+const path = require(path);
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
+  });
+  mainWindow.loadFile(path.join(__dirname, ../build/index.html));
+}
+app.on(ready, createWindow);
